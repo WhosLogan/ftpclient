2c80b311e662b083b25fa8d66751bb3a8fcca493
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-11-06 12:55:41 -0500

    Add connecting and closing to ftp client

--- ftpclient/ftpclient.go
+++ ftpclient/ftpclient.go
@@ -1,13 +1,41 @@
 package ftpclient
 
+import (
+	"errors"
+	"net"
+)
+
 type FtpClient struct {
 	Username string
 	Password string
 	Server   string
+	conn     net.Conn
 }
 
 func NewClient() *FtpClient {
 	return &FtpClient{
 		Username: "anonymous",
+	}
+}
+
+func (c *FtpClient) Connect() error {
+	if c.conn != nil {
+		return errors.New("please close the active connection before opening a new one")
+	}
+
+	conn, err := net.Dial("tcp", c.Server+":21")
+	if err != nil {
+		return err
+	}
+
+	c.conn = conn
+
+	return nil
+}
+
+func (c *FtpClient) Close() {
+	if c.conn != nil {
+		_ = c.conn.Close()
+		c.conn = nil
 	}
 }
