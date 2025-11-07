24a43fa6bdba2b4cf890c54e7a79a5488dbbd8e4
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-11-06 21:15:43 -0500

    Add directory listing and changing

--- ftpclient/actions.go
+++ ftpclient/actions.go
@@ -1,5 +1,10 @@
 package ftpclient
 
+import (
+	"errors"
+	"strconv"
+)
+
 func (c *FtpClient) GetDirectoryList() (string, error) {
 	err := c.connectData()
 	if err != nil {
@@ -11,5 +16,28 @@ func (c *FtpClient) GetDirectoryList() (string, error) {
 		return "", err
 	}
 
-	return string(<-c.dataChan), nil
+	dir := <-c.dataChan
+
+	_, _ = c.readData() // Discard second response
+
+	return string(dir), nil
+}
+
+func (c *FtpClient) ChangeDirectory(path string) error {
+	data, err := c.sendCommand("CWD " + path)
+	if err != nil {
+		return err
+	}
+
+	status, err := strconv.Atoi(string(data[0:3]))
+	if err != nil {
+		c.conn = nil
+		return errors.New("unable to read status code")
+	}
+
+	if status == 250 {
+		return nil
+	}
+
+	return errors.New("unable to change directory")
 }
