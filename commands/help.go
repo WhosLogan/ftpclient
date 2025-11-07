3a6dfab2f72c62cd330979bbc29352b24e105156
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-11-07 09:04:12 -0500

    Reuse string builder for all commands in the help command

--- commands/help.go
+++ commands/help.go
@@ -10,8 +10,8 @@ var helpCommand = &Cmd{
 	name:        "help",
 	description: "Provides a help menu",
 	execute: func(client *ftpclient.FtpClient, arg string, commands *[]*Cmd) {
+		builder := strings.Builder{}
 		for _, command := range *commands {
-			builder := strings.Builder{}
 			builder.WriteString(command.name)
 			if command.argName != "" {
 				builder.WriteString(" <")
@@ -21,7 +21,7 @@ var helpCommand = &Cmd{
 			builder.WriteString(": ")
 			builder.WriteString(command.description)
 			builder.WriteRune('\n')
-			fmt.Print(builder.String())
 		}
+		fmt.Print(builder.String())
 	},
 }
