5b09f6d656c4655724f260860c9d9bb4f6eb0d7d
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-11-07 10:45:42 -0500

    Separate reading data from the data connection init

--- ftpclient/data.go
+++ ftpclient/data.go
@@ -8,28 +8,8 @@ import (
 	"strings"
 )
 
-func (c *FtpClient) connectData() error {
-	data, err := c.sendCommand("PASV")
-	if err != nil {
-		return err
-	}
-
-	status, err := strconv.Atoi(string(data[0:3]))
-	if err != nil {
-		return errors.New("unable to read status code from server")
-	}
-
-	if status != statusEnteringPassive {
-		return errors.New("unable to enter passive mode")
-	}
-
-	ipAndPorts := strings.Split(strings.Split(strings.Split(string(data), ")")[0], "(")[1], ",")
-	ip := ipAndPorts[0] + "." + ipAndPorts[1] + "." + ipAndPorts[2] + "." + ipAndPorts[3]
-	portPart1, _ := strconv.Atoi(ipAndPorts[4])
-	portPart2, _ := strconv.Atoi(ipAndPorts[5])
-	port := portPart1*256 + portPart2
-
-	conn, err := net.Dial("tcp", ip+":"+strconv.Itoa(port))
+func (c *FtpClient) readDataConnection() error {
+	conn, err := c.getDataConnection()
 	if err != nil {
 		return err
 	}
@@ -41,6 +21,35 @@ func (c *FtpClient) connectData() error {
 	go c.handleConnection()
 
 	return nil
+}
+
+func (c *FtpClient) getDataConnection() (net.Conn, error) {
+	data, err := c.sendCommand("PASV")
+	if err != nil {
+		return nil, err
+	}
+
+	status, err := strconv.Atoi(string(data[0:3]))
+	if err != nil {
+		return nil, errors.New("unable to read status code from server")
+	}
+
+	if status != statusEnteringPassive {
+		return nil, errors.New("unable to enter passive mode")
+	}
+
+	ipAndPorts := strings.Split(strings.Split(strings.Split(string(data), ")")[0], "(")[1], ",")
+	ip := ipAndPorts[0] + "." + ipAndPorts[1] + "." + ipAndPorts[2] + "." + ipAndPorts[3]
+	portPart1, _ := strconv.Atoi(ipAndPorts[4])
+	portPart2, _ := strconv.Atoi(ipAndPorts[5])
+	port := portPart1*256 + portPart2
+
+	conn, err := net.Dial("tcp", ip+":"+strconv.Itoa(port))
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
 
 func (c *FtpClient) handleConnection() {
