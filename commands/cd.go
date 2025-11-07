7371ae550c0c846ec43ea82b03d419edd82f4233
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-11-07 10:46:34 -0500

    Validate the client is connected for the cd command

--- commands/cd.go
+++ commands/cd.go
@@ -10,6 +10,10 @@ var cdCommand = &Cmd{
 	description: "Changes the current working directory",
 	argName:     "directory",
 	execute: func(client *ftpclient.FtpClient, arg string, commands *[]*Cmd) {
+		if !client.IsConnected() {
+			fmt.Println("You are not connected to a server")
+		}
+
 		err := client.ChangeDirectory(arg)
 		if err != nil {
 			fmt.Println("Unable to change directory")
