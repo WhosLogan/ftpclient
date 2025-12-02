7436aef8380592a554f337b507de3256dee4eaeb
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-12-01 23:57:33 -0500

    Add basic file browser

--- frontend/src/pages/browser.tsx
+++ frontend/src/pages/browser.tsx
@@ -1,49 +1,95 @@
-import {useEffect} from "preact/hooks";
-import {useState} from "preact/compat";
-import {ListDirectory} from "../../wailsjs/go/main/App";
+import {useEffect, useState} from "preact/hooks";
+import {ChangeDirectory, Close, ListDirectory} from "../../wailsjs/go/main/App";
 import {ftpclient} from "../../wailsjs/go/models";
 import FtpEntry = ftpclient.FtpEntry;
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {useLocation} from "preact-iso";
 
 export function BrowserPage(props: any) {
     const [dir, setDir] = useState<FtpEntry[]>([]);
+    const location = useLocation();
 
     useEffect(() => {
-        console.log("ddasdasd")
-        ListDirectory().then(a => {
-            setDir(a);
-        });
+        refresh().then();
     }, []);
+
+    const disconnect = async () => {
+        await Close();
+        location.route('/');
+    }
+
+    const cd = async (path: string) => {
+        try {
+            await ChangeDirectory(path);
+        } catch (e) {
+            console.error(e);
+        }
+        await refresh()
+        console.log(dir);
+    }
+
+    const refresh = async () => {
+        try {
+            setDir(await ListDirectory());
+        } catch (e) {
+            console.error(e);
+        }
+    }
 
     return (
         <>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Name</TableCell>
-                            <TableCell align="right">Directory</TableCell>
-                            <TableCell align="right">Permissions</TableCell>
-                            <TableCell align="right">Size</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {dir.map((entry) => (
+            <div className="flex flex-col gap-4" style={{height: "calc(100vh - 128px)"}}>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell align="right">Directory</TableCell>
+                                <TableCell align="right">Permissions</TableCell>
+                                <TableCell align="right">Size</TableCell>
+                                <TableCell align="right">Actions</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
                             <TableRow
-                                key={entry.Name}
+                                key={'..'}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                                 <TableCell component="th" scope="row">
-                                    {entry.Name}
+                                    ..
                                 </TableCell>
-                                <TableCell align="right">{entry.IsDir ? "True" : "False"}</TableCell>
-                                <TableCell align="right">{entry.Perms}</TableCell>
-                                <TableCell align="right">{entry.IsDir ? "N/A" : entry.Size}</TableCell>
+                                <TableCell align="right">True</TableCell>
+                                <TableCell align="right">N/A</TableCell>
+                                <TableCell align="right">N/A</TableCell>
+                                <TableCell align="right">
+                                    <Button variant="contained" onClick={() => cd("..")}>Open</Button>
+                                </TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+
+                            {dir.map((entry) => (
+                                <TableRow
+                                    key={entry.Name}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        {entry.Name}
+                                    </TableCell>
+                                    <TableCell align="right">{entry.IsDir ? "True" : "False"}</TableCell>
+                                    <TableCell align="right">{entry.Perms}</TableCell>
+                                    <TableCell align="right">{entry.IsDir ? "N/A" : entry.Size}</TableCell>
+                                    <TableCell align="right">
+                                        {entry.IsDir ? <Button variant="contained" onClick={() => cd(entry.Name)}>Open</Button> :
+                                            <Button variant="contained">Download</Button>}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+
+            <Button variant="contained" onClick={disconnect}>Disconnect</Button>
+            <Button variant="contained" onClick={refresh}>Refresh</Button>
         </>
     )
 }
