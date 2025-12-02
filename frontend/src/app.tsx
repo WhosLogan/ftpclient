138cb1f5c1cc2c29126bdb3d9f36734658065a16
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-12-01 21:14:30 -0500

    Add material ui

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,13 +2,23 @@ import './App.css'
 import {h} from 'preact';
 import {LocationProvider, Router} from "preact-iso";
 import {HomePage} from "./pages/home";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 
 export function App(props: any) {
     return (
-        <LocationProvider>
-            <Router children={[
-                <HomePage path="/" />
-            ]} />
-        </LocationProvider>
+        <ThemeProvider theme={darkTheme}>
+            <LocationProvider>
+                <Router children={[
+                    <HomePage path="/" />
+                ]} />
+            </LocationProvider>
+        </ThemeProvider>
     )
 }
