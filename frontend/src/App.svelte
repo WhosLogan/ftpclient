326cc84ba109c7d483f1fc19010d398ef6f2f407
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-12-09 23:23:10 -0500

    Add Flowbite and basic connection page

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,18 @@
 <script>
     import Connect from "./components/Connect.svelte";
     import Browser from "./components/Browser.svelte";
+    import {EventsOn} from "../wailsjs/runtime/runtime.js";
 
     let connected = $state(false);
+
+    EventsOn("connection-change", (connected) => {
+        connected = connected;
+    });
 </script>
 
-<main>
+<main class="select-none">
     {#if !connected}
-        <Connect />
+        <Connect onConnect={() => connected = true} />
     {:else}
         <Browser />
     {/if}
---
+++ frontend/src/components/Connect.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import { Button, Input, Checkbox, Modal, P } from 'flowbite-svelte';
+    import {Connect} from "../../wailsjs/go/main/App";
+
+    let anon = $state(false);
+    let host = $state('');
+    let username = $state('');
+    let password = $state('');
+    let error = $state('');
+    let open = $state(false);
+
+    let {onConnect} = $props();
+
+    async function connect() {
+        try {
+            await Connect(host, username, password, anon);
+            onConnect();
+        } catch (e) {
+            error = e;
+            open = true;
+        }
+    }
+</script>
+
+<div class="h-screen flex justify-center items-center">
+    <div class="flex flex-col space-y-3 md:w-1/3">
+        <h1 class="text-2xl text-center text-white">FTP Client</h1>
+        <Input bind:value={host} placeholder="Host" />
+        <Input bind:value={username} disabled={anon} placeholder="Username" />
+        <Input bind:value={password} disabled={anon} placeholder="Password" type="password" />
+        <Checkbox bind:checked={anon}>Use Anonymous</Checkbox>
+        <Button onclick={() => connect()}>Connect</Button>
+    </div>
+</div>
+
+<Modal title="An error has occurred" form bind:open={open}>
+    <P>{error}</P>
+
+    {#snippet footer()}
+        <Button type="submit">Ok</Button>
+    {/snippet}
+</Modal>
