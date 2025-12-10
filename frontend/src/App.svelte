206b6cb47768c6596c5841ad52fc0c1b46d3fd2c
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2025-12-09 22:11:25 -0500

    Show connect or browser based on state

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,14 @@
 <script>
+    import Connect from "./components/Connect.svelte";
+    import Browser from "./components/Browser.svelte";
+
+    let connected = $state(false);
 </script>
 
 <main>
-    <p>Hello World</p>
+    {#if !connected}
+        <Connect />
+    {:else}
+        <Browser />
+    {/if}
 </main>
